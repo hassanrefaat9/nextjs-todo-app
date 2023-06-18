20eed6d7b784cb472be605b72a6c54f2dc6e87a6
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-06-18 22:13:11 +0300

    first commit

---
+++ prisma/migrations/20230618160045_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "complete" BOOLEAN NOT NULL,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAT" DATETIME NOT NULL
+);
+
